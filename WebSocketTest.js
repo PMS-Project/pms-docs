e1fb4d1e263455625e402446ffd4cfe563a20e8f
Benjamin Zeller <zeller.benjamin@web.de>
2012-02-04 11:04:54 +0100

    Multiple Requests

--- WebSocketTest.js
+++ WebSocketTest.js
@@ -1,8 +1,13 @@
+var ws = null;
 function test()
 {
+  if(ws != null){
+    ws.send("NOCHMAL");
+    return;
+  }
   if ("WebSocket" in window){
   //  // Google example code
-    var ws = new WebSocket("ws://localhost:3000");
+    ws = new WebSocket("ws://localhost:3000");
     ws.onopen = function(){
         // Web Socket is connected. You can send data by send() method
         ws.send("Dies ist ein Test");
